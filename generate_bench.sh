54bb82f2daf237d1cc631e1949edd0550b288507
ianertson <sebbekarlsson97@gmail.com>
2021-01-29 17:31:39 +0100

    implements CSS support

--- generate_bench.sh
+++ generate_bench.sh
@@ -1,7 +1,7 @@
 OLD=$(pwd)
 BENCHPATH=$OLD/benchmark
 cd benchmark
-yarn add esbuild
+# yarn add esbuild
 virtualenv -p /usr/bin/python3.9 ./venv
 $BENCHPATH/venv/bin/python setup.py develop
 cd $OLD
