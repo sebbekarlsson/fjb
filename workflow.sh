569c7e2204a7243cdf415167defebd071873f92a
ianertson <sebbekarlsson97@gmail.com>
2021-02-11 20:42:53 +0100

    update workflow

--- workflow.sh
+++ workflow.sh
@@ -8,6 +8,16 @@ cd $P
 
 mkdir -p $HOME/.local/lib && mkdir -p $HOME/.local/bin && mkdir -p $HOME/.local/include
 
+cd ./external/hashmap
+make
+make install
+cd $P
+
+cd ./external/libjson
+make
+make install
+cd $P
+
 make install
 
 cd $P
