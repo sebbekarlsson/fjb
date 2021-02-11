1e2fd9e3d6f41c0c625ed3d133adee0e820dc997
ianertson <sebbekarlsson97@gmail.com>
2021-02-11 19:24:55 +0100

    update workflow

--- pull_deps_github.sh
+++ pull_deps_github.sh
@@ -5,8 +5,17 @@ sudo apt-get install --yes gcc
 sudo apt-get install --yes g++
 
 ./pull_deps.sh
-sudo cp external/hashmap/*.a /usr/local/lib/.
+
+sudo mkdir -p /usr/local
+sudo mkdir -p /usr/local/lib
+sudo mkdir -p /usr/local/include
+sudo mkdir -p /usr/local/bin
+
+sudo cp external/hashmap/*.a /usr/local/lib
 sudo cp -r external/hashmap/src/include /usr/local/include/hashmap
 
-sudo cp external/libjson/*.a /usr/local/lib/.
+sudo cp external/libjson/*.a /usr/local/lib
 sudo cp -r external/libjson/src/include /usr/local/include/libjson
+
+cp -r external/hashmap/*.a tests/.
+cp -r external/libjson/*.a tests/.
