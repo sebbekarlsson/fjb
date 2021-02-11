754e29733c176876d2f35025ed813a670afbcdf6
ianertson <sebbekarlsson97@gmail.com>
2021-02-11 19:09:38 +0100

    update workflow

--- pull_deps_github.sh
+++ pull_deps_github.sh
@@ -1,3 +1,9 @@
+sudo apt-get update -y
+sudo apt-get install --yes build-essential
+sudo apt-get install --yes cmake
+sudo apt-get install --yes gcc
+sudo apt-get install --yes g++
+
 ./pull_deps.sh
 cp external/hashmap/*.a /usr/local/lib/.
 cp -r external/hashmap/src/include /usr/local/include/hashmap
