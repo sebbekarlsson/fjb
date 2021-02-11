581ef251c455829cd9bcf124d11547f2763dbea3
ianertson <sebbekarlsson97@gmail.com>
2021-02-11 19:28:37 +0100

    update submoule

--- pull_deps.sh
+++ pull_deps.sh
@@ -1,16 +1,22 @@
 echo "Pulling dependencies..."
+git submodule init
+git submodule update
+git submodule sync
 git submodule update --init --recursive
 git clone https://github.com/sebbekarlsson/gpp.git --recurse-submodules
 
 echo "Building dependency: gpp"
 cd gpp && make
 cd ../
-mv gpp/a.out ./gpp.out
+cp gpp/a.out ./gpp.out
+cp gpp/a.out ./tests/gpp.out
 
 echo "Building dependecy: libhashmap"
 cd ./external/hashmap
 make
+cd ..
 
 echo "Building dependecy: libjson"
 cd ./external/libjson
 make
+cd ..
