35e03877eac98f05c59741a798fbb50f9aca1bd6
Sebastian Karlsson <ianertson@gmail.com>
2021-09-14 20:12:31 +0200

    workflow should install cmake

--- workflow.sh
+++ workflow.sh
@@ -6,8 +6,10 @@ sudo apt install build-essential libssl-dev libffi-dev python-dev --yes
 sudo apt install python3-pip --yes
 # Step 3: Use pip to install virtualenv
 sudo pip3 install virtualenv
-
 sudo apt install gcc wget python-virtualenv --yes
+sudo apt install cmake --yes
+
+
 wget https://nodejs.org/dist/v14.15.5/node-v14.15.5-linux-x64.tar.xz
 tar xf node-v14.15.5-linux-x64.tar.xz
 export PATH="$P/node-v14.15.5-linux-x64/bin:$PATH"
@@ -24,22 +26,22 @@ export PATH="$HOME/.local/bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
 
 
-cd ./external/hashmap
-make
-make install
-cd $P
+# cd ./external/hashmap
+# make
+# make install
+# cd $P
 
-cd ./external/libjson
-make
-make install
-cd $P
+#cd ./external/libjson
+#make
+#make install
+#cd $P
 
-cd ./external/scss
-make
-make install
-cd $P
+#cd ./external/scss
+#make
+#make install
+#cd $P
 
-make install
+#make install
 
 cd $P
 
